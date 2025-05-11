9e04a233c315f5bfb95ec6596df49163d0d41b27
sandroparuna44 <sandroparuna@gmail.com>
2025-05-12 01:36:39 +0400

    HomePage set-up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
+import { Routes, Route } from "react-router"
+import HomePage from "./Pages/HomePage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <h1>Hello world</h1>
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+    </Routes>
+  )
 }
 
 export default App
