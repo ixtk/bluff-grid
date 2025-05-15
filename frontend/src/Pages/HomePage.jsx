2b9f77545b9396299806799690331d983f7fcee6
sandroparuna44 <sandroparuna@gmail.com>
2025-05-15 11:15:10 +0400

    HomePage set-up

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,36 @@
 function HomePage() {
   return (
-    <>
+    <div className="container">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap');
+      </style>
       <h1>Home</h1>
-      <button className="btn btn-primary ">Click Me</button>
-      <button className="btn btn-outline ">Click Me</button>
-    </>
+      <div className="card">
+        <button className="btn btn-primary">Click Me</button>
+        <button className="btn btn-outline">Click Me</button>
+        <button className="btn btn-secondary">Click Me</button>
+      </div>
+
+      <br />
+      <div className="text card ">
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="story">Story</label>
+          <textarea rows={5} id="story"></textarea>
+        </div>
+      </div>
+
+      <br />
+      <span className="badge badge-danger">TRUE</span>
+      <span className="badge badge-success">FALSE</span>
+    </div>
   )
 }
 export default HomePage
