7087eae9ba84f7c9ee7e1d9ae5612490ca58fbb1
palela2009 <alexandre26062009@gmail.com>
2025-05-30 22:44:55 +0400

    I accomplished many tasks, including improving CSS and JSX, but I believe it still requires significant changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import { Routes, Route } from "react-router"
 import HomePage from "./Pages/HomePage"
+import Leaderboard from "./Pages/Leaderboard/Leaderboard";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+       <Route path="/leaderboard" element={<Leaderboard />} />
     </Routes>
   )
 }
