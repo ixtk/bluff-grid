5ee21bb2ef208541b81fc1868a039f17acc66933
palela2009 <alexandre26062009@gmail.com>
2025-06-18 15:27:09 +0400

    did correct config

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyCbIdie0w-i1B9zIg2Ia2APlbzpf__5_fc",
+  authDomain: "bluff-grid.firebaseapp.com",
+  projectId: "bluff-grid",
+  storageBucket: "bluff-grid.firebasestorage.app",
+  messagingSenderId: "234841094709",
+  appId: "1:234841094709:web:53226412b4fb9bbd658f7c"
 };
 
 const app = initializeApp(firebaseConfig);
