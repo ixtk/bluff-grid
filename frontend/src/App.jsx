d3d4189d529405a2f377afc6ac4e5c30cc9eb1e6
vakogabiani <vakogabiani@gmail.com>
2025-06-22 22:02:08 +0400

    home page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import { Routes, Route } from "react-router"
+import { Home } from "./Home/Home"
 import HomePage from "./Pages/HomePage"
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index element={<Home />} />
+      <Route path="/HomePage" element={<HomePage />} />
     </Routes>
   )
 }
